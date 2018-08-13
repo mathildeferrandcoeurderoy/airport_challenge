d65ce1391fc59638c40aff04d6e8acafb798f5f8
Mathilde Ferrand Coeurderoy <mathilde.ferrand.coeurderoy@gmail.com>
2018-08-13 10:25:28 +0100

    add edge cases and all unit tests passing

--- lib/airport.rb
+++ lib/airport.rb
@@ -4,7 +4,7 @@ DEFAULT_SIZE = 3
 
 class Airport
 
-  attr_reader :capacity
+  attr_reader :capacity, :planes, :weather
 
   def initialize(capacity = DEFAULT_SIZE, weather: Weather.new)
     @planes = []
@@ -15,11 +15,13 @@ class Airport
   def land(plane)
     raise "Plane can't land: stormy weather!" if @weather.stormy?
     raise "Plane can't land: airport is full!" if @planes.length >= @capacity
+    raise "Plane can't land: already at the airport!" if @planes.include?(plane)
     @planes << plane
   end
 
   def take_off(plane)
     raise "Plane can't take off: stormy weather!" if @weather.stormy?
-    @planes.pop
+    raise "Plane is already flying!" unless @planes.include?(plane)
+    @planes.delete(plane)
   end
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -8,42 +8,62 @@ describe Airport do
     end
   end
 
-  context 'when no stormy' do
-    before do
-      allow(subject).to receive(:stormy).and_return false
-    end
-
+  context 'when not stormy' do
     describe "#land method" do
-      it "responds to land_in method" do
+      it "responds to land method" do
         expect(subject).to respond_to(:land).with(1).arguments
       end
 
       it "will not allow a plane to land if airport full" do
+        allow(subject.weather).to receive(:stormy?).and_return false
         plane = Plane.new
-        DEFAULT_SIZE.times { subject.land(plane) }
+        DEFAULT_SIZE.times { subject.land(Plane.new) }
         expect { subject.land(plane) }.to raise_error("Plane can't land: airport is full!")
+      end
+
+      it "verifies if a landed plane is at the airport" do
+        allow(subject).to receive(:stormy?).and_return false
+        plane = Plane.new
+        subject.land(plane)
+        expect(subject.planes.include?(plane)).to eq true
+      end
+
+      it "verifies that a landed plane cannot land again" do
+        allow(subject).to receive(:stormy?).and_return false
+        plane = Plane.new
+        subject.land(plane)
+        expect { subject.land(plane) }.to raise_error("Plane can't land: already at the airport!")
       end
     end
   end
 
   context 'when stormy' do
     it "will not allow a plane to land if there is a storm" do
-      allow(subject).to receive(:stormy?).and_return true
+      allow(subject.weather).to receive(:stormy?).and_return true
       plane = Plane.new
       expect { subject.land(plane) }.to raise_error("Plane can't land: stormy weather!")
     end
   end
 
   describe "#take_off method" do
-     it "responds to take_off" do
-       expect(subject).to respond_to(:take_off).with(1).arguments
-     end
+    it "responds to take_off" do
+      expect(subject).to respond_to(:take_off).with(1).arguments
+    end
 
     context 'when stormy' do
       it "will not allow a plane to take off if there is a storm" do
+        allow(subject.weather).to receive(:stormy?).and_return true
         plane = Plane.new
         expect { subject.take_off(plane) }.to raise_error("Plane can't take off: stormy weather!")
       end
+    end
+
+    it "verifies if a plane is already flying" do
+      allow(subject).to receive(:stormy?).and_return false
+      plane = Plane.new
+      subject.land(plane)
+      subject.take_off(plane)
+      expect { subject.take_off(plane) }.to raise_error("Plane is already flying!")
     end
 
   end
